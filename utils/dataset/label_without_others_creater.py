b4de6694120beb053ff5143255569eeb9c88d052
unknown <firat.saritas@students.fhnw.ch>
2022-05-19 21:23:52 +0200

    create label_without_others.py

---
+++ utils/dataset/label_without_others_creater.py
@@ -0,0 +1,27 @@
+import pickle
+import sys
+sys.path.append('../')
+
+
+label_translate_1 = {
+    'Rost/Strassenrost': 0,
+    'Vollguss/Pickelloch belueftet': 1,
+    'Gussbeton/Pickelloch geschlossen': 2,
+    'Vollguss/Pickelloch geschlossen': 3,
+    'Gussbeton/Pickelloch belueftet': 4,
+    'Vollguss/Handgriff geschlossen': 5,
+    'Gussbeton/Handgriff seitlich': 6,
+    'Rost/Einlauf rund': 7,
+    'Rost/Strassenrost gewoelbt': 8,
+    'Vollguss/Aufklappbar': 9,
+    'Gussbeton/Handgriff mitte': 10,
+    'Vollguss/Handgriff geschlossen, verschraubt': 11
+}
+
+def label_save(label_translate, name):
+    with open(name, 'wb') as f:
+        pickle.dump(label_translate, f)
+
+if __name__ == '__main__':
+    label_save(label_translate_1,'label_without_others.pkl')
+    
